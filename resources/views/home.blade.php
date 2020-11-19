31b8e38e0d644f6f224d2cce633ff5c061d06d61
Octavio Oyanedel <octavio.oyanedel@gmail.com>
2020-11-19 00:07:36 -0300

    En proceso maquetación adminlte, icono menú y enlaces desplegados en nav lateral.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,116 +5,73 @@
 
       <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <a class="navbar-brand" href="#">SIND1</a>
-<!-- Left navbar links -->
-<ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-    </li>
-    <li class="nav-item">
-        <a href="#" class="nav-link">Home</a>
-    </li>
-    <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-    </li>
 
-</ul>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-chevron-circle-right"></i></a>
+        </li>
+    </ul>
 
-<!-- Formulario buscar -->
-<ul class="navbar-nav ml-auto">
-    <li>
-        <a class="nav-link" data-toggle="dropdown" href="#" title="N° Socios">
-            <i class="fas fa-user-friends"></i>
-            <span class="badge badge-danger navbar-badge">500</span>
-          </a>
-    </li>
-<li class="nav-item">
-    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-        Hola: {{ Auth::user()->name }}
-    </a>
+    <ul class="navbar-nav ml-auto">
+        <li>
+            <a class="nav-link" data-toggle="dropdown" href="#" title="N° Socios">
+                <i class="fas fa-user-friends"></i>
+                <span class="badge badge-danger navbar-badge">500</span>
+            </a>
+        </li>
 
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
-    </div>
-</li>
-</ul>
+    </ul>
 
 
 </nav>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-<!-- Brand Logo -->
-<a href="#" class="brand-link">
-    <img src="\img\logo_blanco.png" alt="Logo SIND1" class="brand-image img-circle elevation-3" style="opacity: 1">
-    <span class="brand-text font-weight-light">SIND1 </span><i style="margin-top: 6px;" data-widget="pushmenu" role="button" class="fas fa-bars float-right"></i>
-</a>
 
-<!-- Sidebar -->
-<div class="sidebar">
-<!-- Sidebar user panel (optional) -->
-<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-<div class="image">
-{{-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> --}}
-</div>
+    <a href="#" class="brand-link">
+        <img src="\img\logo_blanco.png" alt="Logo SIND1" class="brand-image img-circle elevation-3" style="opacity: 1">
+        <span class="brand-text font-weight-light">SIND1 </span><i style="margin-top: 6px;" data-widget="pushmenu" role="button" class="fas fa-chevron-circle-left float-right"></i>
+    </a>
 
-</div>
+    <div class="sidebar">
 
-<!-- Sidebar Menu -->
-<nav class="mt-2">
-<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-<!-- Add icons to the links using the .nav-icon class
-with font-awesome or any other icon font library -->
-<li class="nav-item has-treeview menu-open">
-<a href="#" class="nav-link active">
-<i class="nav-icon fas fa-tachometer-alt"></i>
-<p>
-Dashboard
-<i class="right fas fa-angle-left"></i>
-</p>
-</a>
-<ul class="nav nav-treeview">
-<li class="nav-item">
-<a href="#" class="nav-link active">
-<i class="far fa-circle nav-icon"></i>
-<p>Dashboard v1</p>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="far fa-circle nav-icon"></i>
-<p>Dashboard v2</p>
-</a>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="far fa-circle nav-icon"></i>
-<p>Dashboard v3</p>
-</a>
-</li>
-</ul>
-</li>
 
-<li class="nav-item">
-<a href="#" class="nav-link">
-<i class="nav-icon far fa-circle text-info"></i>
-<p>Informational</p>
-</a>
-</li>
-</ul>
-</nav>
-<!-- /.sidebar-menu -->
-</div>
-<!-- /.sidebar -->
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item has-treeview menu-open">
+                    <a href="#" class="nav-link active">
+                        <p>Socios<i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active"><p>Cargas</p></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"><p>Estudios</p></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link"><p>Socios</p></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        Salir
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+
 </aside>
-<!-- Content Wrapper. Contains page content -->
-    {{-- FILA LOGIN --}}
-    <livewire:login />
+
+
 </div>
 @endsection
